24c20eeae37b9fe0586bb4597848449e76b077e6
Okma <c.amko@ufl.edu>
2015-09-24 11:13:08 -0400

    Main Page 0.1

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,38 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
+angular.module('core').controller('MainController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.subjects =
+    [
+    	{
+    		name: "Biology",
+    		img: "modules/core/client/img/subject_icons/biology.png",
+    		description: "Biology is COOL."
+    	},
+    	{
+    		name: "Genetics",
+    		img: "modules/core/client/img/subject_icons/biology.png",
+    		description: "Biology is COOL."
+    	},
+    	{
+    		name: "Biotility ftw",
+    		img: "modules/core/client/img/subject_icons/biology.png",
+    		description: "Biology is COOL."
+    	},
+    	{
+    		name: "Chemistry",
+    		img: "modules/core/client/img/subject_icons/biology.png",
+    		description: "Biology is COOL."
+    	},
+    	{
+    		name: "Chemistry",
+    		img: "modules/core/client/img/subject_icons/biology.png",
+    		description: "Biology is COOL."
+    	}
+    ];
+
   }
 ]);
