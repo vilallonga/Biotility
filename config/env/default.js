bb8b6d89ce55d0873c95e66adbc2e6a90191c9b0
Okma <c.amko@ufl.edu>
2015-09-27 11:51:36 -0400

    Database Setup

--- config/env/default.js
+++ config/env/default.js
@@ -3,7 +3,7 @@
 module.exports = {
   app: {
     title: 'Biotility',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    description: 'Preparing students for biometrics exam.',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
--- config/lib/mongoose.js
+++ config/lib/mongoose.js
@@ -4,41 +4,79 @@
  * Module dependencies.
  */
 var config = require('../config'),
-  chalk = require('chalk'),
-  path = require('path'),
-  mongoose = require('mongoose');
+    chalk = require('chalk'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
 
 // Load the mongoose models
-module.exports.loadModels = function () {
-  // Globbing model files
-  config.files.server.models.forEach(function (modelPath) {
-    require(path.resolve(modelPath));
-  });
+module.exports.loadModels = function() {
+    // Globbing model files
+    config.files.server.models.forEach(function(modelPath) {
+        require(path.resolve(modelPath));
+    });
 };
+
+// Manually defining the schemas like a normal human being.
+var studentSchema = new Schema({
+    firstname: {type: String, required: 'Please enter your first name.'},
+    lastname: {type: String, required: 'Please enter your last name.'},
+    email: {type: String, required: 'Please enter your email address.', set: toLower},
+    coursesTakingIds: [Number]
+});
+
+var teacherSchema = new Schema({
+    firstname: {type: String, required: 'Please enter your first name.'},
+    lastname: {type: String, required: 'Please enter your last name.'},
+    email: {type: String, required: 'Please enter your email address.', set: toLower},
+    coursesTeachingIds: [Number]
+});
+
+var questionSchema = new Schema({
+    description: {type : String, required: true},
+    type: {type: String, required: true},
+    answerDesc1: {type: String, required: true},
+    answerDesc2: {type: String, required: true},
+    answerDesc3: {type: String},
+    answerDesc4: {type: String},
+    correctAnswer: {type: String, required: true}
+});
+
+module.exports.Student = mongoose.model('Student', studentSchema);
+module.exports.Teacher = mongoose.model('Teacher', teacherSchema);
+module.exports.Question = mongoose.model('Question', questionSchema);
+
+
+function toLower(email) {
+  return email.toLowerCase();
+}
+
 
 // Initialize Mongoose
-module.exports.connect = function (cb) {
-  var _this = this;
+module.exports.connect = function(cb) {
+    var _this = this;
 
-  var db = mongoose.connect(config.db.uri, config.db.options, function (err) {
-    // Log Error
-    if (err) {
-      console.error(chalk.red('Could not connect to MongoDB!'));
-      console.log(err);
-    } else {
+    var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
+        // Log Error
+        if (err) {
+            console.error(chalk.red('Could not connect to MongoDB!'));
+            console.log(err);
+        } else {
 
-      // Enabling mongoose debug mode if required
-      mongoose.set('debug', config.db.debug);
+            // Enabling mongoose debug mode if required
+            mongoose.set('debug', config.db.debug);
 
-      // Call callback FN
-      if (cb) cb(db);
-    }
-  });
+            // Call callback FN
+            if (cb) cb(db);
+        }
+    });
 };
 
-module.exports.disconnect = function (cb) {
-  mongoose.disconnect(function (err) {
-    console.info(chalk.yellow('Disconnected from MongoDB.'));
-    cb(err);
-  });
+
+
+module.exports.disconnect = function(cb) {
+    mongoose.disconnect(function(err) {
+        console.info(chalk.yellow('Disconnected from MongoDB.'));
+        cb(err);
+    });
 };
