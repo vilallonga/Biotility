881be0ded45d12c08a6d200fc5b82cb9a75ebe3f
David <dyeung93@hotmail.com>
2015-10-29 11:27:03 -0400

    directive for csv

---
+++ modules/quiz/client/directives/parseCSV.client.directives.js
@@ -0,0 +1,25 @@
+'use strict';
+//Directive used to return data in json
+
+angular.module('quiz').directive('onReadCSV', function ($parse) {
+	//console.log("in on read file");
+	return {
+		restrict: 'A',
+		scope: false,
+		link: function(scope, element, attrs) {
+            var fn = $parse(attrs.onReadFile);
+            
+			element.on('change', function(onChangeEvent) {
+				var reader = new FileReader();
+                
+				reader.onload = function(onLoadEvent) {
+					scope.$apply(function() {
+						fn(scope, {$fileContent:onLoadEvent.target.result});
+					});
+				};
+
+				reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+			});
+		}
+	};
+});
