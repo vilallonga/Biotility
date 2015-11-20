4dc188ab3dd911260dba56cf537d8175db8299c2
Okma <c.amko@ufl.edu>
2015-11-20 09:44:16 -0500

    mongo path fixing for production

--- config/env/test.js
+++ config/env/test.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-test',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://devteam:devteam@ds027769.mongolab.com:27769/software5c',
     options: {
-      user: '',
-      pass: ''
+      user: 'devteam',
+      pass: 'devteam'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
