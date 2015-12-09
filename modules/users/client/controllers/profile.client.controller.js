bb130a94305f726b4ed858cd0c118f0ab2ba8253
acolas1 <acolas1@ufl.edu>
2015-12-08 19:30:44 -0500

    pop ups and comments

--- modules/users/client/controllers/profile.client.controller.js
+++ modules/users/client/controllers/profile.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//controler for teacher page retrieval of students
+//controler for teacher page list retrieval of students
 angular.module('users').controller('StudentListController', ['$rootScope', '$scope', '$state', '$location', '$filter', '$http', 'Authentication',
     function($rootScope, $scope, $state, $location, $filter, $http, Authentication) {
     $scope.$state = $state;
@@ -22,7 +22,7 @@ angular.module('users').controller('StudentListController', ['$rootScope', '$sco
 
    
 
-   
+   //gets the name from the param list
  	$scope.getName = function(disName) {
            $scope.user = disName.userName;
            $scope.email = disName.email;
@@ -33,18 +33,23 @@ angular.module('users').controller('StudentListController', ['$rootScope', '$sco
         };
     }
 ]);
-
+//controller for the student teacher page
 angular.module('users').controller('StudentGetController', ['$rootScope', '$scope', '$state', '$stateParams', '$location', '$filter', '$http', 'Authentication',
     function($rootScope, $scope, $state, $stateParams, $location, $filter, $http, Authentication) {
    
     $scope.authentication = Authentication;
    // $scope.section = null;
+   //testing
    console.log("in state params:");
+   //testing
    console.log($stateParams.username);
+   //pass to scope
     $scope.userFinal = $stateParams.username;
     $scope.emailFinal = $stateParams.email;
     $scope.firstnameFinal = $stateParams.firstname;
     $scope.lastnameFinal = $stateParams.lastname;
+
+    //testing
    console.log("Second controller");
    console.log($scope.userFinal);
    console.log($scope.email);
